5029a239d0a987d59b99b2fb8c10b2bf84362b87
Olexyy <olexyy.mails@gmail.com>
2018-02-09 23:46:08 +0200

    Changed examples.

--- src/Plugin/HookInfo/ExampleHookInfo.php
+++ src/Plugin/HookInfo/ExampleHookInfo.php
@@ -9,41 +9,44 @@ use Drupal\hook_manager\Plugin\HookInfoBase;
  * Example hook info.
  *
  * Be careful: 'id' should be unique in project scope, else plugins override.
+ * I propose set module name as identifier.
  * In 'hooks' array we define 'canonical' hook name, that begins with 'hook_'.
  * Conventional for methods, that implement hooks, is that they should be
  * camel case analogs of 'canonical' hook name.
  * Value in 'hooks' array is priority compare to other plugins for this hook.
  *
  * @HookInfo(
- *  id = "example_hooks_2",
+ *  id = "hook_manager",
  *  hooks = {
+ *    "hook_theme" = 0,
  *    "hook_form_alter" = 0,
- *    "hook_token_info" = 10,
- *    "hook_block_build_alter" = 0,
+ *    "hook_token_info" = 0,
  *  },
  * )
  */
-class Example2HookInfo extends HookInfoBase {
+class ExampleHookInfo extends HookInfoBase {
 
   /**
    * Implements hook_token_info().
    */
   public function hookTokenInfo() {
-    $a = 1;
+    // $a = 1;
+  }
+
+  /**
+   * Implements hook_theme().
+   */
+  public function hookTheme() {
+    /*return  [
+      'theme2' => [],
+    ];*/
   }
 
   /**
    * Implements hook_form_alter().
    */
   public function hookFormAlter(array &$form, FormStateInterface $formState, $formId) {
-    $form['foo'] = 'bar';
-  }
-
-  /**
-   * Implements hook_query_alter().
-   */
-  public function hookBlockBuildAlter(&$data, &$context) {
-    $a = $data;
+    // $form['foo'] = 'bar';
   }
 
 }
